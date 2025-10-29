1d84023bf455951e35e69c07942f7938e504a2aa
ghernandezsotodev <ghernandezsoto.dev@gmail.com>
2025-10-29 17:33:13 -0300

    Fix: Eliminar adaptador de Netlify de la configuración y dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   
-  // Le decimos a Astro que construya un sitio 100% estático (SSG)
-  output: 'static',
+  output: 'static', 
   
   integrations: [
-    tailwind(),
-    netlify()
+    tailwind()
   ]
 });
